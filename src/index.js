c59b5ebdb91112410619db426626ce67b817a664
Kyosuke Tsuneda <k_tsuneda@yumemi.co.jp>
2020-06-17 17:22:53 +0900

    install webpack-dev-server

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import _ from 'lodash';
 
 function component() {
   const element = document.createElement('div');
-  const array = ['Hello', 'Webpack'];
-  element.innerHTML = _.join(array, '++');
-  
+  const array = ['Hello', 'Webpack', '!!'];
+  element.innerHTML = _.join(array, '_');
+
   return element;
 }
 
